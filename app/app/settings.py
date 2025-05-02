c4da0c12bf24e0b4a7b118b4a724e75b2376ba46
beni-f <benifissha@gmail.com>
2025-05-02 23:27:42 +0300

    updated settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -156,9 +156,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'account.CustomUser'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
